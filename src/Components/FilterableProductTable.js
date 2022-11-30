5a1a40db4c306bf894664a21d8f51a7f6f3061cc
ilyas Macbook Air <ilyasmgazali@gmail.com>
2022-11-30 13:03:58 +0000

    components

---
+++ src/Components/FilterableProductTable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from './SearchBar.js'
+import ProductTable from './ProductTable.js'
+
+export default function FilterableProductTable() {
+  return (
+    <div>
+       FilterableProductTable:  
+      <SearchBar/>
+     </div>
+  )
+}
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SearchBar() {
+  return (
+    <div>SearchBar: this should have: 1 bar + 1 form/input</div>
+  )
+}
